969c65b74feda002f2477963e9ad5c836ee45a17
Frank Riley <fhriley@gmail.com>
2022-01-01 19:13:03 -0700

    Add tag for kodi name

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ KODI_NAME=Matrix
 USER=fhriley
 IMAGE=kodi-headless-novnc
 
-BUILD_ARGS="--build-arg BUILDKIT_INLINE_CACHE=1 --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg KODI_VER=$KODI_VER --build-arg KODI_NAME=$KODI_NAME"
+BUILD_ARGS="--build-arg BASE_IMAGE=$BASE_IMAGE --build-arg KODI_VER=$KODI_VER --build-arg KODI_NAME=$KODI_NAME"
 PLATFORMS="linux/amd64,linux/arm64/v8,linux/arm/v7"
 CACHE="type=registry,ref=$USER/$IMAGE:latest"
 
 docker login
-docker buildx build $BUILD_ARGS --platform $PLATFORMS --tag $USER/$IMAGE:$KODI_VER --tag $USER/$IMAGE:latest --cache-from $CACHE --cache-to type=inline,mode=max --push --pull .
+docker buildx build $BUILD_ARGS --platform $PLATFORMS --tag $USER/$IMAGE:$KODI_VER --tag $USER/$IMAGE:$KODI_NAME --tag $USER/$IMAGE:latest --cache-from $CACHE --cache-to type=inline,mode=max --push --pull .
