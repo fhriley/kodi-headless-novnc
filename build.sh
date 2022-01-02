9f9b05e9677f628034efc92aa7050a9cfbc59bfb
Frank Riley <fhriley@gmail.com>
2022-01-01 17:29:13 -0700

    use cache

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ KODI_NAME=Matrix
 USER=fhriley
 IMAGE=kodi-headless-novnc
 
-BUILD_ARGS="--build-arg BASE_IMAGE=$BASE_IMAGE --build-arg KODI_VER=$KODI_VER --build-arg KODI_NAME=$KODI_NAME"
+BUILD_ARGS="--build-arg BUILDKIT_INLINE_CACHE=1 --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg KODI_VER=$KODI_VER --build-arg KODI_NAME=$KODI_NAME"
 PLATFORMS="linux/amd64,linux/arm64/v8,linux/arm/v7"
+CACHE="type=registry,ref=$USER/$IMAGE:latest"
 
 docker login
-docker buildx build $BUILD_ARGS --platform $PLATFORMS --tag $USER/$IMAGE:$KODI_VER --tag $USER/$IMAGE:latest --push --pull .
+docker buildx build $BUILD_ARGS --platform $PLATFORMS --tag $USER/$IMAGE:$KODI_VER --tag $USER/$IMAGE:latest --cache-from $CACHE --cache-to type=inline,mode=max --push --pull .
 
 #docker manifest create $USER/$IMAGE:latest $USER/$IMAGE:$KODI_VER
 #docker manifest push --purge $USER/$IMAGE:latest
