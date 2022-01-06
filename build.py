b2f83ff7418e8df1f2fc1c3d92014b34361d23c9
Frank Riley <fhriley@gmail.com>
2022-01-06 15:16:20 -0700

    Add platform to options

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ import shlex
 
 BASE_IMAGE = 'ubuntu:22.04'
 IMAGE_NAME = 'fhriley/kodi-headless-novnc'
-PLATFORMS = ['linux/amd64', 'linux/arm64/v8', 'linux/arm/v7']
+PLATFORMS = ['linux/amd64', 'linux/arm64', 'linux/arm/v7']
 CACHE = f'type=registry,ref={IMAGE_NAME}:'
 BUILDX = 'docker buildx build {build_args} --platform {platforms} {tags} --cache-from {cache} --cache-to type=inline,mode=max {push} --pull .'
 
@@ -13,12 +13,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=f'Build {IMAGE_NAME}')
     parser.add_argument('-x', '--execute', action='store_true',
                         help='execute the build after printing the command')
-    parser.add_argument('-p', '--push', action='store_true',
+    parser.add_argument('--push', action='store_true',
                         help='push the image')
     parser.add_argument('-b', '--base', default=BASE_IMAGE,
                         help=f'the base image (default: "{BASE_IMAGE}" )')
     parser.add_argument('-c', '--cache',
                         help='the tag to cache from (default: first tag argument)')
+    parser.add_argument('-p', '--platform', nargs='+', default=PLATFORMS,
+                        help=f'the platform to build for (default: {PLATFORMS})')
     parser.add_argument('branch', help='the branch/tag to build')
     parser.add_argument('tag', nargs='+', help='a tag to add to the image')
     args = parser.parse_args()
@@ -30,7 +32,7 @@ if __name__ == '__main__':
 
     buildx = BUILDX.format(
         build_args=' '.join([f'--build-arg {arg}' for arg in build_args]),
-        platforms=','.join(PLATFORMS),
+        platforms=','.join(args.platform),
         tags=' '.join([f'--tag {IMAGE_NAME}:{tag}' for tag in args.tag]),
         cache=CACHE + (args.cache or args.tag[0]),
         push='--push' if args.push else ''
