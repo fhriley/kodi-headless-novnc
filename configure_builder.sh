118c0af191f03a4e1501c88a63c0b7fb7fd4fe9a
Frank Riley <fhriley@gmail.com>
2022-02-06 07:41:20 -0700

    update arm v7 builder hostname

--- configure_builder.sh
+++ configure_builder.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 
 docker buildx create --name cluster --platform linux/amd64
-DOCKER_HOST=tcp://arm:2376 docker buildx create --name cluster --append --platform linux/arm/v7
+DOCKER_HOST=tcp://arm32:2376 docker buildx create --name cluster --append --platform linux/arm/v7
 DOCKER_HOST=tcp://arm64:2376 docker buildx create --name cluster --append --platform linux/arm64
 docker buildx inspect --bootstrap cluster
 docker buildx use cluster
